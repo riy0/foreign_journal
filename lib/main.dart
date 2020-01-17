a1c473929733d284d39a780f55b1355b8529545d
riy0 <chac0ptx@gmail.com>
2020-01-18 08:07:36 +0900

    add/screen

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
-import './screens/home_screen.dart';
+import 'package:flutter_calendar_carousel/flutter_calendar_carousel.dart';
+// import './screens/home_screen.dart';
 
 void main() => runApp(MyApp());
 
@@ -12,7 +13,7 @@ class MyApp extends StatelessWidget {
       theme: ThemeData(
         primarySwatch: Colors.cyan,
       ),
-      home: HomeScreen(),
+      home: CalendarCarousel(),
     );
   }
 }
