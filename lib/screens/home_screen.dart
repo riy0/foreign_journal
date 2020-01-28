a6f453e763ca5a5db1a15f86a13ae9f351b2d3ab
riy0 <chac0ptx@gmail.com>
2020-01-28 13:44:07 +0900

    add/simple home screen

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:speech_recognition/speech_recognition.dart';
 
 class HomeScreen extends StatefulWidget {
   @override
@@ -6,8 +7,41 @@ class HomeScreen extends StatefulWidget {
 }
 
 class _HomeScreenState extends State<HomeScreen> {
-//  String baseUrl =
+  //  String baseUrl =
   // 'https:// @api.cloudinary.com/v1_1/demo/resources/image';
+  SpeechRecognition _speechRecognition;
+  bool _isAvailable = false;
+  bool _isListening = false;
+
+  String resultText = "";
+
+  void initState() {
+    super.initState();
+    initSpeechRecognizer();
+  }
+
+  void initSpeechRecognizer() {
+    _speechRecognition = SpeechRecognition();
+    _speechRecognition.setAvailabilityHandler(
+      (bool result) => setState(() => _isAvailable = result),
+    );
+
+    _speechRecognition.setRecognitionStartedHandler(
+      () => setState(() => _isListening = true),
+    );
+
+    _speechRecognition.setRecognitionResultHandler(
+      (String speech) => setState(() => resultText = speech),
+    );
+
+    _speechRecognition.setRecognitionCompleteHandler(
+      () => setState(() => _isListening = false),
+    );
+
+    _speechRecognition.activate().then(
+          (result) => setState(() => _isAvailable = result),
+        );
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -15,7 +49,35 @@ class _HomeScreenState extends State<HomeScreen> {
       appBar: AppBar(
         title: Text('home'),
       ),
-      body: Center(),
+      body: Container(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: <Widget>[
+            Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: <Widget>[
+                FloatingActionButton(
+                  mini: true,
+                  backgroundColor: Colors.lime,
+                  onPressed: () {},
+                ),
+                FloatingActionButton(
+                  onPressed: () {},
+                ),
+                FloatingActionButton(
+                  mini: true,
+                  backgroundColor: Colors.orange,
+                  onPressed: () {},
+                ),
+              ],
+            ),
+            Container(
+              child: Text(resultText),
+            )
+          ],
+        ),
+      ),
     );
   }
 }
