4bc39903ca50bfc41f1cd267a0858d5762608bd4
riy0 <chac0ptx@gmail.com>
2020-01-29 00:38:25 +0900

    update/ use openapi

---
+++ client/UI/js/main.js
@@ -0,0 +1,10 @@
+$("#get-started").on("click", function() {
+  const imgLoc = $("#section").position().top;
+  console.log(imgLoc);
+  $("html, body").animate(
+    {
+      scrollTop: imgLoc
+    },
+    900
+  );
+});
