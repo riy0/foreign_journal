74785a21209c6eab26caaeebcdc43bd84a53ec69
riy0 <chac0ptx@gmail.com>
2020-01-28 23:36:11 +0900

    change structure, start server

--- client/lib/notifiers/user.dart
+++ client/lib/notifiers/user.dart
@@ -1,3 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 import 'package:http/http.dart';
+
+class User with ChangeNotifier{
+  bool _isLoading = false;
+  bool get _isLoading => _isLoadings;
+}
